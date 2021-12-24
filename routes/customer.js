358e0802924b9b1773c76ee53cd7eda93c153988
minhvo0o <vdminh@penphy.edu.vn>
2021-12-24 15:09:11 +0700

    Customer static pages

--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const res = require('express/lib/response')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.send('Customer')
+  res.render('customer/index')
+})
+
+router.get('/editInfo', function (req, res, next) {
+  res.render('customer/editInfo')
+})
+
+router.get('/orderStatus', function (req, res, next) {
+  res.render('customer/orderStatus')
+})
+
+router.get('/order', function (req, res, next) {
+  res.render('customer/order')
 })
 
 module.exports = router
