7d6aa9cf22b206a11c26cd83dd8ac2811bcc26cb
Dai Nguyen <ncdai3651408@gmail.com>
2021-12-24 15:33:14 +0700

    Add pages : mange product, order, income

--- routes/driver.js
+++ routes/driver.js
@@ -5,4 +5,16 @@ router.get('/', function (req, res, next) {
   res.send('Driver')
 })
 
+router.get('/register', function (req, res, next) {
+  res.render('driver/register')
+})
+
+router.get('/manage-order', function (req, res) {
+  res.render('driver/manage-order')
+})
+
+router.get('/manage-income', function (req, res) {
+  res.render('driver/manage-income')
+})
+
 module.exports = router
