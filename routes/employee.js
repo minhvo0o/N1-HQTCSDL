2f8ce16bc2e3ef241dc2fb58440bf7a59140ae89
minhvo0o <vdminh@penphy.edu.vn>
2021-12-24 16:24:42 +0700

    Employee static pages

--- routes/employee.js
+++ routes/employee.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.send('Employee')
+  res.render('employee/index')
+})
+
+router.get('/contractDetail', function (req, res, next) {
+  res.render('employee/contractDetail')
 })
 
 module.exports = router
