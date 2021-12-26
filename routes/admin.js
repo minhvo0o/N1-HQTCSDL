8e9c6a9b30f6b1d2c55c4481dfa77e9f990a7b67
Dai Nguyen <ncdai3651408@gmail.com>
2021-12-26 20:36:33 +0700

    Admin : update layout

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { DriverModel, PartnerModel, CustomerModel, EmployeeModel } = require('../models')
+// const { DriverModel, PartnerModel, CustomerModel, EmployeeModel } = require('../models')
 
-router.get('/', async (req, res, next) => {
-  const drivers = await DriverModel.getDrivers()
-  const partners = await PartnerModel.getPartners()
-  const customers = await CustomerModel.getCustomers()
-  const employees = await EmployeeModel.getEmployees()
-  res.render('admin/index', { drivers: drivers, partners: partners, customers: customers, employees: employees })
+// router.get('/', async (req, res, next) => {
+//   const drivers = await DriverModel.getDrivers()
+//   const partners = await PartnerModel.getPartners()
+//   const customers = await CustomerModel.getCustomers()
+//   const employees = await EmployeeModel.getEmployees()
+//   res.render('admin/index', { drivers: drivers, partners: partners, customers: customers, employees: employees })
+// })
+
+router.get('/manage-users', (req, res) => {
+  res.render('admin/manage-users')
 })
 
 module.exports = router
