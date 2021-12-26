542fca6ed70b9145addb43085c1b25e2ddeae336
minhvo0o <vdminh@penphy.edu.vn>
2021-12-26 14:44:57 +0700

    Driver manage income, Driver manage order

--- models/Customer.js
+++ models/Customer.js
@@ -6,7 +6,7 @@ const getOrders = () => {
   return new Promise((resolve, reject) => {
     // Read all rows from table
     const request = new Request(
-      `SELECT DonHang.MaDH, ChiTietDonHang.MaSP, SanPham.TenSP, ThongTinVanChuyen.TinhTrangVC
+      `SELECT DonHang.MaDH, ChiTietDonHang.MaSP, SanPham.TenSP, ThongTinVanChuyen.TinhTrangVC, DonHang.PhiVC
       FROM DonHang
       INNER JOIN ChiTietDonHang ON DonHang.MaDH = ChiTietDonHang.MaDH
       INNER JOIN SanPham ON ChiTietDonHang.MaSP = SanPham.MaSP
